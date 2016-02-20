478c99e22126c28f5c14ef624c55f3d0b0c391ac
Artem Grechko <artem@intspirit.com>
2016-02-20 15:06:35 +0300

    Added map select to Event

--- wp-content/plugins/event-tickets-extend/index.php
+++ wp-content/plugins/event-tickets-extend/index.php
@@ -96,26 +96,132 @@ class Tribe__Tickets__Main__Extend {
     }
 
     public function hooks() {
-        add_action( 'wp_insert_post', array( $this, 'add_custom_field_to_attendees' ), 10, 3 );
+        add_action( 'wp_insert_post', array( $this, 'prepeare_insert_post' ), 10, 3 );
         add_filter( 'manage_tribe_events_page_tickets-attendees_columns', array( $this, 'add_my_custom_attendee_column'), 20 );
         add_filter( 'tribe_events_tickets_attendees_table_column', array( $this,'populate_my_custom_attendee_column'), 10, 3 );
 
         add_filter( 'tribe_events_register_venue_type_args', array( $this,'tribe_venues_custom_field_support') );
         // add_action( 'tribe_events_single_venue_before_upcoming_events', array( $this,'show_wp_custom_fields') );
+        add_action( 'tribe_after_location_details', array( $this, 'displayEventMapDropdown' ) );
 
-        add_filter('tribe_events_meta_box_template', array($this, 'change_event_mb_tpl'));
+        // add_filter('tribe_events_meta_box_template', array($this, 'change_event_mb_tpl'));
     }
 
 
-    public function change_event_mb_tpl($tpl) {
-        return $this->plugin_path . '/events-meta-box.php';
+    // public function change_event_mb_tpl($tpl) {
+    //     return $this->plugin_path . '/events-meta-box.php';
+    // }
+
+    public function displayEventMapDropdown( $post_id ) {
+        $map_id = get_post_meta( $post_id, '_EventMapID', true );
+        ?>
+        <table>
+            <tbody>
+                <tr>
+                    <td style="width:200px">Choose map:</td>
+                    <td>
+                        <?php
+                        $this->saved_map_dropdown( $map_id );
+                        ?>
+                        <div class="edit-map-link" <?php if ( empty( $map_id ) ) { ?>style="display:none;"<?php } ?>>
+                            <a 
+                                data-admin-url="<?php echo esc_url( admin_url( 'post.php?action=edit&post=' ) ); ?>" 
+                                href="<?php echo esc_url( admin_url( sprintf( 'post.php?action=edit&post=%s', $map_id ) ) ); ?>" 
+                                target="_blank"
+                            ><?php echo esc_html( sprintf( __( 'Edit %s', 'the-events-calendar' ), $this->singular_map_label ) ); ?>
+                            </a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <?php
+    }
+
+    public function saved_map_dropdown( $current = null, $name = 'map[MapID]' ) {
+        global $post;
+        $my_map_ids     = array();
+        $current_user     = wp_get_current_user();
+        $my_maps        = false;
+        $my_map_options = '';
+
+        $selected_map_id = get_post_meta($post->ID, '_map_id', true);
+
+        $my_maps = $this->get_map_info(
+            null,
+            array(
+                'post_status' => array(
+                    'publish',
+                    'draft',
+                    'private',
+                    'pending',
+                ),
+                'author' => $current_user->ID,
+            )
+        );
+
+        if ( ! empty( $my_maps ) ) {
+            foreach ( $my_maps as $my_map ) {
+                $my_map_ids[] = $my_map->ID;
+                $map_title    = wp_kses( get_the_title( $my_map->ID ), array() );
+                $my_map_options .= '<option value="' . esc_attr( $my_map->ID ) . '"';
+                $my_map_options .= selected( $selected_map_id, $my_map->ID, false );
+                $my_map_options .= '>' . $map_title . '</option>';
+            }
+        }
+
+        if ( $my_maps ) {
+            echo '<select class="chosen map-dropdown" style="width: 220px;" name="' . esc_attr( 'map_id' ) . '" id="saved_map">';
+            echo $my_map_options;
+            echo '</select>';
+        } else {
+            echo '<p class="nosaved">' . esc_html__( 'No saved %s exists.') . '</p>';
+        }
+    }
+
+    function get_map_info( $p = null, $args = array() ) {
+        $defaults = array(
+            'post_type'            => 'map_seats',
+            'nopaging'             => 1,
+            'post_status'          => 'publish',
+            'ignore_sticky_posts ' => 1,
+            'orderby'              => 'title',
+            'order'                => 'ASC',
+            'p'                    => $p,
+        );
+
+        $args = wp_parse_args( $args, $defaults );
+        $r    = new WP_Query( $args );
+        if ( $r->have_posts() ) :
+            return $r->posts;
+        endif;
+
+        return false;
+    }
+
+    public function prepeare_insert_post($post_id, $post, $update) {
+        // post_type
+        switch ($post->post_type) {
+            case 'tribe_rsvp_attendees':
+                $this->add_custom_field_to_attendees($post_id, $post, $update);
+                break;
+            case 'tribe_events':
+                $this->add_custom_field_to_events($post_id, $post, $update);
+                break;
+            // case 'map_seats':
+            //     $this->add_custom_field_to_map($post_id, $post, $update);
+            //     break;
+        }
     }
 
     public function add_custom_field_to_attendees( $post_id, $post, $update ) {
-        if (isset($_POST["attendee"])) {
-            $seats = empty( $_POST['attendee']['seats'] ) ? null : sanitize_text_field( $_POST['attendee']['seats'] );
-            update_post_meta( $post_id, 'seats', $seats);
-        }
+        $seats = empty( $_POST['attendee']['seats'] ) ? null : sanitize_text_field( $_POST['attendee']['seats'] );
+        update_post_meta( $post_id, 'seats', $seats);
+    }
+
+    public function add_custom_field_to_events( $post_id, $post, $update ) {
+        $map_id = empty( $_POST['map_id'] ) ? null : sanitize_text_field( $_POST['map_id'] );
+        update_post_meta( $post_id, '_map_id', $map_id);
     }
 
     public function add_my_custom_attendee_column( $columns ) {
